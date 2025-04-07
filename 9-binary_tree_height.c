10739b330e3344fd780a3304bae08d4b7c22026e
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-04-07 14:49:23 +0000

    Get an height of a segment of a binary

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Calculates the height of a binary tree
+ * @tree: Pointer to the root node of the tree to calculate the height
+ *
+ * Return: The height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (1 + left_height);
+	else
+		return (1 + right_height);
+}
